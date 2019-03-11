9f6322a891c0586412f0f95dfc1257a66e75909a
Shaneli2 <276433574@qq.com>
2019-03-11 21:33:30 +0800

    Initial commit

---
+++ src/main/java/com/shane/App.java
@@ -0,0 +1,13 @@
+package com.shane;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
